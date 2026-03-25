8cc55072b116cae83341ab0c3ed317b8d12471b7
Varshitha596 <koilakondavarshapraveenkumar@gmail.com>
2026-03-25 22:34:43 +0530

    js

--- new.jsx
+++ new.jsx
@@ -1,2 +1,39 @@
 //to create elememt 
 const newy=React.createElement("h1");
+
+//function 
+function App() {
+  const name="me";
+  const task="learning react";
+  return(
+<div>
+  <h1>
+    hello {name}
+  </h1>
+  <p>and my task is {task}</p>
+</div>
+  );
+}
+
+export default App;
+
+//component 
+function App() {
+  const name="me";
+  const task="learning react";
+  return(
+<div>
+    const good=() => {
+return (
+    <div>
+         <h1>
+    hello {name}
+  </h1>
+  <p>and my task is {task}</p>
+</div>
+    </div>
+);
+    
+ 
+
+} 
