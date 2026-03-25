2da095cdbaef96e9ee70b61fb343dec38f9e8890
Varshitha596 <koilakondavarshapraveenkumar@gmail.com>
2026-03-25 23:46:09 +0530

    props

--- new.jsx
+++ new.jsx
@@ -1,61 +1,73 @@
 //to create elememt 
-const newy=React.createElement("h1");
+// const newy=React.createElement("h1");
 
-//function 
-function App() {
-  const name="me";
-  const task="learning react";
-  return(
-<div>
-  <h1>
-    hello {name}
-  </h1>
-  <p>and my task is {task}</p>
-</div>
-  );
-}
+// //function 
+// function App() {
+//   const name="me";
+//   const task="learning react";
+//   return(
+// <div>
+//   <h1>
+//     hello {name}
+//   </h1>
+//   <p>and my task is {task}</p>
+// </div>
+//   );
+// }
 
-export default App;
+// export default App;
 
 //component 
-function App() {
-  const name="me";
-  const task="learning react";
-  return(
-<div>
-    const good=() => {
-return (
-    <div>
-         <h1>
-    hello {name}
-  </h1>
-  <p>and my task is {task}</p>
-</div>
-    </div>
-);
+// function App() {
+//   const name="me";
+//   const task="learning react";
+//   return(
+// <div>
+//     const good=() => {
+// return (
+//     <div>
+//          <h1>
+//     hello {name}
+//   </h1>
+//   <p>and my task is {task}</p>
+// </div>
+//     </div>
+// );
     
  
 
-} 
+// } 
 
 
-function App() {
-  const name="me";
-  const task="learning react";
-  //nrml function
-  const gettask = ()=> {
-    return "Learning";
-  };
-  //component
-  const Greeting=()=>{
-    return <h2>hi{name} and {task}</h2>;
-  };
-return (
-  <div>
-    <Greeting/>
-    <p>{gettask()}</p>
-  </div>
-);
-}
+// function App() {
+//   const name="me";
+//   const task="learning react";
+//   //nrml function
+//   const gettask = ()=> {
+//     return "Learning";
+//   };
+//   //component
+//   const Greeting=()=>{
+//     return <h2>hi{name} and {task}</h2>;
+//   };
+// return (
+//   <div>
+//     <Greeting/>
+//     <p>{gettask()}</p>
+//   </div>
+// );
+// }
 
-export default App;
+// export default App;
+
+//
+//props
+
+// function App({name,task}) {
+//   return <h1>{name} hello yoo {task}</h1>
+//  }
+//  function Welcome(){
+//   return <App name="varsha" task="going"/>
+//  }
+
+// export default Welcome;
